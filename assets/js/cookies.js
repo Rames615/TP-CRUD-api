c7a5ff8158e0a049413a0a47855d6a341c6e606d
Rames615 <ramostrick@gmx.com>
2025-04-29 13:48:41 +0200

    button accep and reject cookie is added to the page cookie

--- assets/js/cookies.js
+++ assets/js/cookies.js
@@ -1,28 +1,39 @@
-   document.cookie = "nom=Stéphane";
-   let cookie = document.cookie;
-    console.log(cookie);
+//    document.cookie = "nom=Stéphane";
+//    let cookie = document.cookie;
+//     console.log(cookie);
 
-    const paragraphe = document.querySelector('p');
-    console.log(paragraphe);
+//     const paragraphes = document.querySelector('p');
+//     console.log(paragraphes);
 
-    if (cookie) {
-        paragraphe.innerHTML = `il y a un cookie, ce cookie comporte ces valeurs ${cookie}`;
-    } else {
-        paragraphe.innerHTML = `il n\'y a pas de cookie pour le gouté`;
-    }
-
-//     function setCookie(name, value, days) {
-//         let expires = "";
-//         if (days) {
-//             const date = new Date();
-//             date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-//             expires = "; expires=" + date.toUTCString();
-//         }
-//         document.cookie = name + "=" + (value || "") + expires + "; path=/";
+//     if (cookie) {
+//         paragraphes.innerHTML = `il y a un cookie, ce cookie comporte ces valeurs ${cookie}`;
+//     } else {
+//         paragraphes.innerHTML = `il n\'y a pas de cookie pour le gouté`;
 //     }
-    
-// // Exemple d'utilisation
-// setCookie("utilisateur", "JeanDupont", 7); // Crée un cookie nommé "utilisateur" avec la valeur "JeanDupont" qui expire dans 7 jours
+    const modal = document.getElementById("cookieModal");
+    const acceptBtn = document.getElementById('acceptBtn');
+    const refuseBtn = document.getElementById('refuseBtn');
+    const checkbox = document.getElementById('acceptCheckbox');
+    const paragraphe = document.querySelector('p');
+
+    // Accept button logic
+    acceptBtn.addEventListener('click', () => {
+        if (checkbox.checked) {
+            document.cookie = "nom=Rames; path=/;";
+            paragraphe.innerHTML = "Cookie accepté et enregistré : " + document.cookie;
+            modal.style.display = "none";
+        } else {
+            paragraphe.innerHTML = "Veuillez cocher la case pour accepter les cookies.";
+        }
+    });
+
+    // Refuse button logic
+    refuseBtn.addEventListener('click', () => {
+        document.cookie = "nom=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+        paragraphe.innerHTML = "Cookie refusé et supprimé.";
+        modal.style.display = "none";
+    });
+
 
     
     
